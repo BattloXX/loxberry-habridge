08075f3b5c1fbef68eced28448794dd9ad767b3a
BattloXX <battlogg.johannes@gmail.com>
2020-12-04 19:29:31 +0100

    Update to 5.3.1RC3

--- preupgrade.sh
+++ preupgrade.sh
@@ -52,7 +52,7 @@ cp $5/data/plugins/$3/data/device.db /tmp/p3-device.db
 
 echo "<INFO> Getting habridge Sources from https://github.com"
 rm /opt/loxberry/data/plugins/p3_lox_habridge/ha-bridge.jar
-/usr/bin/wget --progress=dot:mega -t 10 -O /opt/loxberry/data/plugins/p3_lox_habridge/ha-bridge.jar https://github.com/bwssytems/ha-bridge/releases/download/v5.3.0/ha-bridge-5.3.0.jar
+/usr/bin/wget --progress=dot:mega -t 10 -O /opt/loxberry/data/plugins/p3_lox_habridge/ha-bridge.jar https://github.com/bwssytems/ha-bridge/releases/download/v5.3.1RC3/original-ha-bridge-5.3.1RC3.jar
 if [ ! -f /opt/loxberry/data/plugins/p3_lox_habridge/ha-bridge.jar ]; then
     echo "<FAIL> Something went wrong while trying to download habridge Sources."
     exit 1
